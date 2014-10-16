4353591f069852161b2b38b234d9f610cd216e19
vasilevrv <vasilev@fastvps.ru>
2014-10-16 12:53:32 +0400

    Update sslv3.sh
    Fix CentOS

--- sslv3.sh
+++ sslv3.sh
@@ -8,7 +8,7 @@ if [[ "$SSLSERV" == "apache2" ]]; then
 	echo 'Create symlink /etc/apache2/mods-enabled/ssl.conf.. Done!'
      fi
      sed -i 's|SSLProtocol all -SSLv2|SSLProtocol All -SSLv2 -SSLv3|g' /etc/apache2/mods-enabled/ssl.conf
-     echo 'Fix /etc/apache2/mods-enabled/ssl.conf.. Done!'
+     echo 'Fix /etc/apache2/mods-enabled/ssl.conf .. Done!'
      apache2ctl -t 2>&1 > /dev/null
      if [ $? == 0 ]; then
         /etc/init.d/apache2 restart
@@ -17,8 +17,8 @@ if [[ "$SSLSERV" == "apache2" ]]; then
      fi
 
 elif [[ "$SSLSERV" == "httpd" ]]; then
-     echo -e '<IfModule ssl_module>\nSSLProtocol All -SSLv2 -SSLv3\n</IfModule>' > /etc/httpd/conf.d/sslfix.conf
-     echo 'Create /etc/httpd/conf.d/sslfix.conf.. Done!'
+     sed -i 's|SSLProtocol All -SSLv2|SSLProtocol All -SSLv2 -SSLv3|g' /etc/httpd/conf.d/ssl.conf
+     echo 'Fix /etc/httpd/conf.d/ssl.conf .. Done!'
      apachectl -t 2>&1 > /dev/null
      if [ $? == 0 ]; then
         /etc/init.d/httpd restart
@@ -34,7 +34,7 @@ elif [[ "$SSLSERV" == "nginx" ]]; then
         apt-get install nginx && echo 'Nginx updated!'
     fi
     echo 'ssl_protocols TLSv1 TLSv1.1 TLSv1.2;' > /etc/nginx/conf.d/sslfix.conf
-    echo 'Create /etc/nginx/conf.d/sslfix.conf.. Done!'
+    echo 'Create /etc/nginx/conf.d/sslfix.conf .. Done!'
     nginx -t 2>&1 > /dev/null
     if [ $? == 0 ]; then
         /etc/init.d/nginx restart
